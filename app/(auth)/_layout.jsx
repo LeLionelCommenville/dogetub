d3c0df658c45e6cf7a2d1db77fdb269ca7e569a6
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-05-06 18:25:38 +0200

    feat: create the basic structure and file for the project

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,13 @@
+import AuthLact from 'react'
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>Auth</Text>
+    </View>
+  )
+}
+
+export default AuthLayout
