12c0ac9e8be628d3fb691d74d9ee7f41adc3f93c
Lionel Commenvillle <lionelcommenville@gmail.com>
2024-05-09 22:01:55 +0200

    feat: add some image and a image index for futher use

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white font-sans">
       <Text>Dogetube like youtube but much wow!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className="text-3xl font-sans-bold">Go to Profile</Link>
+      <Link href="/home" className="text-3xl font-sans-bold">Go to Home</Link>
     </View>
   );
 }
---
+++ constants/images.js
@@ -0,0 +1,4 @@
+import doge from "../assets/images/path.png";
+import logo from "../assets/images/logo.jpg";
+
+export default { logo, doge };
